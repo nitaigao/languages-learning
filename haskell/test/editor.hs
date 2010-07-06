b8b2b473859334354ee952b07ec948cdaf6ce488
Nicholas Kostelnik <nkostelnik@gmail.com>
2010-07-06 02:04:42 +0100

    kinda broken it but in a good way

--- haskell/test/editor.hs
+++ haskell/test/editor.hs
@@ -7,24 +7,26 @@ render col (rows, cols, (x:xs)) = do
   render (column + 1) (rows, cols, xs)
 
 coords input = map (\x -> (read x::Int)) (tail (words input))
-create image input = let (x:y:_) = coords input in ready (x, y, take (x * y) (repeat "0"))
+create image input = let (x:y:_) = coords input in (x, y, take (x * y) (repeat "0"))
 
 color_coord (cols, rows, image) input = do
   let (x:y:_) = coords input
   let (command:_) = words input
   let (color) = last input
   let index = x + (y * cols)
-  ready (cols, rows, concat [take index image, [[color]], drop (index + 1) image])
+  (cols, rows, concat [take index image, [[color]], drop (index + 1) image])
 
 commands = [("I", create), ("L",color_coord)]
 
 ready image = do
   putStrLn "Enter a command:"
   input <- getLine
-  if input == "S"
+  let (command:_) = filter (\x -> fst x == head (words input)) commands
+  let image' = (snd command) image input
+  if input == "S" 
     then render 0 image
-      else let (command:_) = filter (\x -> fst x == head (words input)) commands in (snd command) image input
-  ready image
+    else return () 
+  ready image'
 
 main = do
   ready (0,0,[])
