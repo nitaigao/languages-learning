32dbbf8f4611483dfef5c0a88b6b79bfea225fa6
Nicholas Kostelnik <nkostelnik@gmail.com>
2010-07-06 01:08:27 +0100

    seperated render from the rest of the commands

--- haskell/test/editor.hs
+++ haskell/test/editor.hs
@@ -1,20 +1,13 @@
 module Main where
 
-draw image input = do
-  (render(0, image))
-
-render (col,(rows, cols,[])) = do putStr "\n\n"
-render (col,(rows,cols,(x:xs))) = do
+render col (rows, cols, []) = do putStr "\n\n"
+render col (rows, cols, (x:xs)) = do
   let column = if col == cols then 0 else col  
   putStr (if column == 0 then "\n" ++ x else x)
-  render (column + 1,(rows, cols, xs))
+  render (column + 1) (rows, cols, xs)
 
 coords input = map (\x -> (read x::Int)) (tail (words input))
-
-create image input = do
-  let (x:y:_) = coords input
-  putStrLn ("Created a new image of dimensions " ++ show x ++ " by " ++ show y)
-  ready (x, y, take (x * y) (repeat "0"))
+create image input = let (x:y:_) = coords input in ready (x, y, take (x * y) (repeat "0"))
 
 color (cols, rows, image) input = do
   let (x:y:_) = coords input
@@ -22,13 +15,16 @@ color (cols, rows, image) input = do
   let index = x + (y * cols)
   ready (cols, rows, concat [take index image, ["d"], drop (index + 1) image])
 
-commands = [("I", create), ("S", draw), ("L",color)]
+commands = [("I", create), ("L",color)]
 
 ready image = do
   putStrLn "Enter a command:"
   input <- getLine
-  let (command:_) = filter (\x -> fst x == head (words input)) commands in (snd command) image input
-  (ready image)
+  if input == "S"
+    then render 0 image
+    else let (command:_) = filter (\x -> fst x == head (words input)) commands in (snd command) image input
+  ready image
 
 main = do
-  (ready (0,0,[]))
+  ready (0,0,[])
+  return ()
