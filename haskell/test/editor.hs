bc5198419086bd1458051a4e1f8030798c5d2947
Nicholas Kostelnik <nkostelnik@gmail.com>
2010-07-06 01:19:17 +0100

    correct color at coords

--- haskell/test/editor.hs
+++ haskell/test/editor.hs
@@ -9,20 +9,21 @@ render col (rows, cols, (x:xs)) = do
 coords input = map (\x -> (read x::Int)) (tail (words input))
 create image input = let (x:y:_) = coords input in ready (x, y, take (x * y) (repeat "0"))
 
-color (cols, rows, image) input = do
+color_coord (cols, rows, image) input = do
   let (x:y:_) = coords input
   let (command:_) = words input
+  let (color) = last input
   let index = x + (y * cols)
-  ready (cols, rows, concat [take index image, ["d"], drop (index + 1) image])
+  ready (cols, rows, concat [take index image, [[color]], drop (index + 1) image])
 
-commands = [("I", create), ("L",color)]
+commands = [("I", create), ("L",color_coord)]
 
 ready image = do
   putStrLn "Enter a command:"
   input <- getLine
   if input == "S"
     then render 0 image
-    else let (command:_) = filter (\x -> fst x == head (words input)) commands in (snd command) image input
+      else let (command:_) = filter (\x -> fst x == head (words input)) commands in (snd command) image input
   ready image
 
 main = do
