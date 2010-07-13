8f80844b69936062353b0ed041ad97e5f3c25b3f
nick kostelnik <nkostelnik@gmail.com>
2010-07-13 07:56:19 +0100

    horizontal and vertical

--- haskell/test/editor.hs
+++ haskell/test/editor.hs
@@ -28,11 +28,36 @@ render (Image x y d) = do
 
 pixel input (Image x y d) = do
   let (cx:cy:_) = coords input
-  let (command:_) = words input
   let (color) = last input : []
   let index = cx + (cy * x)
   Image x y (concat [(take index d), [color], (drop (index + 1) d)])
 
+color_pixels_y color cx (cy:cys) (Image x y d) = do
+  let index = cx + (cy * x)
+  let image = Image x y (concat [(take index d), [color], (drop (index + 1) d)])
+  color_pixels_y color cx cys image
+
+color_pixels_y color cx [] image = image
+
+vertical input image = do
+  let (cx:y1:y2:_) = coords input
+  let (color) = last input : []
+  let rows = [y1..y2]
+  color_pixels_y color cx rows image  
+  
+color_pixels_x color cy (cx:cxs) (Image x y d) = do
+  let index = cx + (cy * x)
+  let image = Image x y (concat [(take index d), [color], (drop (index + 1) d)])
+  color_pixels_x color cy cxs image
+
+color_pixels_x _ _ [] image = image
+
+horizontal input image = do
+  let (cy:x1:x2:_) = coords input
+  let (color) = last input : []
+  let columns = [x1..x2]
+  color_pixels_x color cy columns image
+    
 ready image = do 
   putStrLn "Enter a command: "
   input <- getLine
@@ -46,6 +71,10 @@ ready image = do
       ready image
     "L" -> do
       ready image' where image' = pixel input image
+    "V" -> do
+      ready image' where image' = vertical input image
+    "H" -> do
+      ready image' where image' = horizontal input image
     _   -> do 
       unknown
       ready image
